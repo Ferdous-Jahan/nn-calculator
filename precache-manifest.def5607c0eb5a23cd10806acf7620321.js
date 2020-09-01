4cb951d8c4c21fcd9e0619d90cb8352e65e5d4bd
Ferdous Jahan <ferdousjahan2@gmail.com>
2020-09-01 18:29:08 +0600

    Updates

---
+++ precache-manifest.def5607c0eb5a23cd10806acf7620321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee147acc85184b4a36b51ceed311b0a",
+    "url": "/nn-calculator/index.html"
+  },
+  {
+    "revision": "624c18a0480a9887da34",
+    "url": "/nn-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3533823d9ffcb76882b8",
+    "url": "/nn-calculator/static/js/2.e2cd29e8.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/nn-calculator/static/js/2.e2cd29e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624c18a0480a9887da34",
+    "url": "/nn-calculator/static/js/main.3e619d50.chunk.js"
+  },
+  {
+    "revision": "00f8b8a0bae49a8fcd14",
+    "url": "/nn-calculator/static/js/runtime-main.678806d3.js"
+  }
+]);
